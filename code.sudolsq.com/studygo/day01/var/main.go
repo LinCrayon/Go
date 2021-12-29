9da5cb3fed5d4870b07aa6561fb78e6bc6c498d8
Tame-complexity <linshengqian@outlook.com>
2021-12-29 23:03:42 +0800

    go

--- code.sudolsq.com/studygo/day01/var/main.go
+++ code.sudolsq.com/studygo/day01/var/main.go
@@ -6,9 +6,9 @@ import "fmt"
 
 //声明变量
 //变量声明格式为：var 变量名 变量类型
-	//var  name string
-	//var age int
-	//var isOk bool
+//var  name string
+//var age int
+//var isOk bool
 
 //批量声明
 var (
@@ -22,10 +22,10 @@ func main() {
 	age = 18
 	isOk = true
 	//Go语言中非全局变量声明后必须使用，不使用就编译不过去
-	fmt.Print(isOk)             //打印
+	fmt.Print(isOk) //打印
 	fmt.Println()
 	fmt.Printf("name:%s\n", name) //Printf格式化，%s是占位符，使用name值去替换
-	fmt.Println(age)            //打印完指点内容后在后面加一个换行符号
+	fmt.Println(age)              //打印完指点内容后在后面加一个换行符号
 
 	fmt.Println("===================================================")
 
@@ -44,11 +44,12 @@ func main() {
 
 	fmt.Println("==============================")
 
-	//匿名变量（anonymous variable）。 匿名变量用一个下划线_表示
+	//匿名变量（anonymousvariable）。 匿名变量用一个下划线_表示
 	//匿名变量不占用命名空间，不会分配内存，所以匿名变量之间不存在重复声明
 
 	//1、函数外的每个语句都必须以关键字开始（var、const、func等）
 	//2、:=不能使用在函数外。
 	//3、_多用于占位，表示忽略值。
 }
+
 //同一个作用域（{}）中不能声明同名的变量
